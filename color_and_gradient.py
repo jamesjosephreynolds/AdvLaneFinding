a46e5179ef0b45f2e109832dda00f6c09f7d33e4
Jim Reynolds <jamesjosephreynolds@gmail.com>
2017-02-07 21:44:33 -0500

    changed behavior for '__main__'

--- color_and_gradient.py
+++ color_and_gradient.py
@@ -5,57 +5,21 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import calibrate_camera
 
-def do_wrapper():
-    sobel_thresh = [20, 100]
-    s_thresh = [170, 255]
-    src = []
-    dst = do(src, sobel_thresh, s_thresh, suppress = False)
-
 def do(src, sobel_thresh, s_thresh, suppress = True):
     if suppress is False:
         print('Apply Sobel and color thresholds')
 
-        # load test images
-        fname_array = ['test_images/straight_lines1.jpg',
-                 'test_images/straight_lines2.jpg',
-                 'test_images/test1.jpg',
-                 'test_images/test2.jpg',
-                 'test_images/test3.jpg',
-                 'test_images/test4.jpg',
-                 'test_images/test5.jpg',
-                 'test_images/test6.jpg']
+    # Sobel value
+    sobel_binary = SobelX(src, sobel_thresh)
 
-    if suppress is False:
-        for fidx in range(len(fname_array)):
-            src = mpimg.imread(fname_array[fidx], format = 'jpg')
+    # S value
+    color_binary = HlsGrad(src, s_thresh)
             
-            # Sobel value
-            sobel_binary = SobelX(src, sobel_thresh)
+    stack= np.dstack((np.zeros_like(color_binary), 255*sobel_binary, 255*color_binary))
+    dst = np.zeros_like(color_binary)
+    dst[(sobel_binary == 1) | (color_binary == 1)] = 1
 
-            # S value
-            color_binary = HlsGrad(src, s_thresh)
-            
-            stack= np.dstack((np.zeros_like(color_binary), 255*sobel_binary, 255*color_binary))
-            dst = np.zeros_like(color_binary)
-            dst[(sobel_binary == 1) | (color_binary == 1)] = 1
-            plt.subplot(131),plt.imshow(src),plt.title('Input')
-            plt.subplot(132),plt.imshow(stack),plt.title('Stacked')
-            plt.subplot(133),plt.imshow(dst,cmap = 'gray'),plt.title('Composite')
-            filestr = 'output_images/color_and_gradient'+str(fidx)+'.png'
-            plt.savefig(filestr,format='png')
-            print('Created '+filestr)
-
-    else:
-        # Sobel value
-        sobel_binary = SobelX(src, sobel_thresh)
-        # S value
-        color_binary = HlsGrad(src, s_thresh)
-          
-        dst = np.zeros_like(color_binary)
-        dst[(sobel_binary == 1) | (color_binary == 1)] = 1
-
-
-    return dst
+    return dst, stack
 
 def SobelX(src, sobel_thresh):
     gray = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
@@ -78,7 +42,27 @@ def HlsGrad(src, s_thresh):
 if __name__ == '__main__':
     # Run the color and sobel threshold function on test images
     print('Running locally on test images, printing and saving allowed')
-    do_wrapper()
+    sobel_thresh = [20, 100]
+    s_thresh = [170, 255]
+    fname_array = ['test_images/straight_lines1.jpg',
+                 'test_images/straight_lines2.jpg',
+                 'test_images/test1.jpg',
+                 'test_images/test2.jpg',
+                 'test_images/test3.jpg',
+                 'test_images/test4.jpg',
+                 'test_images/test5.jpg',
+                 'test_images/test6.jpg']
+    for fidx in range(len(fname_array)):
+        src = mpimg.imread(fname_array[fidx], format = 'jpg')
+
+        dst, stack = do(src, sobel_thresh, s_thresh, suppress = False)
+
+        plt.subplot(131),plt.imshow(src),plt.title('Input')
+        plt.subplot(132),plt.imshow(stack),plt.title('Stacked')
+        plt.subplot(133),plt.imshow(dst,cmap = 'gray'),plt.title('Composite')
+        filestr = 'output_images/color_and_gradient'+str(fidx)+'.png'
+        plt.savefig(filestr,format='png')
+        print('Created '+filestr)    
     
 else:
     # Run the color and sobel threshold function on image argument
