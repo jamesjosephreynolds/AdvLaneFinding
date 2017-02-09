b40d7ba7f62fce9c9825546b4fb5d7fb3da4bc66
Jim Reynolds <jamesjosephreynolds@gmail.com>
2017-02-08 19:50:47 -0500

    add file

---
+++ lane_find_files.py
@@ -0,0 +1,39 @@
+def cal_images():
+    # Images for camera calibration
+
+    fname_array = ['camera_cal/calibration1.jpg',
+                 'camera_cal/calibration2.jpg',
+                 'camera_cal/calibration3.jpg',
+                 'camera_cal/calibration4.jpg',
+                 'camera_cal/calibration5.jpg',
+                 'camera_cal/calibration6.jpg',
+                 'camera_cal/calibration7.jpg',
+                 'camera_cal/calibration8.jpg',
+                 'camera_cal/calibration9.jpg',
+                 'camera_cal/calibration10.jpg',
+                 'camera_cal/calibration11.jpg',
+                 'camera_cal/calibration12.jpg',
+                 'camera_cal/calibration13.jpg',
+                 'camera_cal/calibration14.jpg',
+                 'camera_cal/calibration15.jpg',
+                 'camera_cal/calibration16.jpg',
+                 'camera_cal/calibration17.jpg',
+                 'camera_cal/calibration18.jpg',
+                 'camera_cal/calibration19.jpg',
+                 'camera_cal/calibration20.jpg']
+
+    return fname_array
+
+def test_images():
+    # Images for testing overall pipeline
+
+    fname_array = ['test_images/straight_lines1.jpg',
+                 'test_images/straight_lines2.jpg',
+                 'test_images/test1.jpg',
+                 'test_images/test2.jpg',
+                 'test_images/test3.jpg',
+                 'test_images/test4.jpg',
+                 'test_images/test5.jpg',
+                 'test_images/test6.jpg']
+
+    return fname_array
